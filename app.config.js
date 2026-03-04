a629f22d4f04957c2240a4d0223facfb56b9d523
juan obrach <juanobrach@gmail.com>
2026-03-03 22:46:54 -0300

    feat: Introduce shared `expo-config` package and refactor app configurations to use `createAppConfig`.

--- app.config.js
+++ app.config.js
@@ -1,35 +1,24 @@
-module.exports = {
-  name: 'zig-zag',
-  slug: 'zig-zag',
+const { createAppConfig } = require('@eb-packages/expo-config');
+
+module.exports = createAppConfig({
+  name: 'zigzag',
+  slug: 'zigzag',
   version: '1.0.0',
-  updates: {
-    url: 'https://u.expo.dev/34a46d03-0540-481a-8326-ea123a330635',
-  },
-  runtimeVersion: '1.0.0',
-  extra: {
-    googleMapsApiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY,
-    eas: {
-      projectId: '34a46d03-0540-481a-8326-ea123a330635',
-    },
-  },
-  assetBundlePatterns: ['**/*'],
-  web: {
-    bundler: 'metro',
-    config: {
-      googleMapsApiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY,
-    },
-  },
+  projectId: '34a46d03-0540-481a-8326-ea123a330635',
+  plugins: ['expo-router'],
   ios: {
-    bundleIdentifier: 'com.entitiybuilders.zig-zag',
     infoPlist: {
       NSLocationWhenInUseUsageDescription:
         'Usamos tu ubicación para mostrar actividades cercanas a vos.',
     },
   },
   android: {
-    package: 'com.juanobrach.zigzag',
     permissions: ['ACCESS_COARSE_LOCATION', 'ACCESS_FINE_LOCATION'],
   },
-  plugins: ['expo-router'],
-  newArchEnabled: true,
-};
+  web: {
+    bundler: 'metro',
+  },
+  extra: {
+    googleMapsApiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY,
+  },
+});
